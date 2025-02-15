ba716640658728be085941f9ac4f34d06561091a
Yang <yangzhang.john@gmail.com>
2025-02-15 13:05:26 +0000

    update

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) { // add to the 'request' event.
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello World</h1>');
-}).listen(80, "0.0.0.0");
+}).listen(8080, "0.0.0.0");
